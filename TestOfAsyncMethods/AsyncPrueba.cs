76b9b4e635d7f4d6f6ba0eb19c6900b12ced1a8b
Rodolfo Reina Figueroa <33300721+rodolforeinafigueroa@users.noreply.github.com>
2021-11-29 14:48:39 -0500

    Add files via upload
    Initial upload

---
+++ TestOfAsyncMethods/AsyncPrueba.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOfAsyncMethods
+{
+    internal class AsyncPrueba
+    {
+        private List<int> integers;
+       internal AsyncPrueba()
+        {
+            integers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+        }
+
+
+        public async Task Insert()
+        {
+            var resultList = new List<int>();
+            var random = new Random();
+            //var tasks = new List<Task>();
+            //foreach (int integer in integers)
+            //{
+            //    tasks.Add(AddToResultList(integer, resultList, random));
+
+            //}
+
+            var tasks = integers.Select(integer => AddToResultList(integer, resultList, random));
+            await Task.WhenAll(tasks);
+            foreach (var result in resultList)
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+       
+
+        private static async Task AddToResultList(int integer, List<int> resultList, Random random)
+        {
+            var waitTime = random.Next(3000, 10000);
+            Console.WriteLine(waitTime);
+            await Task.Delay(waitTime);
+            resultList.Add(integer);
+        }
+
+
+    }
+}
---
+++ TestOfAsyncMethods/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestOfAsyncMethods;
+
+var prueba = new AsyncPrueba();
+await prueba.Insert();
